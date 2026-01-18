92f22aa2b10b6720c7292e171d83201889e3542c
Gleb Iudin <smartgleb@gmail.com>
2026-01-19 01:52:01 +0300

    next config modified

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.randomuser.me",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        pathname: "/**",
-      },
-    ],
+    domains: ["randomuser.me"],
   },
 };
 
